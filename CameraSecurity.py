7c495e45815053862019d8de8d71a315c143e431
eddie-semarts <eddie.thomas@semanticarts.com>
2023-01-10 06:29:28 -0700

    Update the camera security file

--- CameraSecurity.py
+++ CameraSecurity.py
@@ -1,3 +1,9 @@
+import asyncio
+import RPi.GPIO as GPIO
+
+from datetime import datetime, timedelta
+
+
 class CameraSecurity:
     """Security system built with a DORHEA Camera Module w/ night-vision compatibility, and an HC-SR501 PIR Sensor module
     for motion detection.
@@ -10,10 +16,87 @@ class CameraSecurity:
 
         Args:
             motion_sensor_pin_number (int): Meant to be the RPi.GPIO.BCM pin number.
+
+        TODO:
+        - Set up camera
+        - Set up motion sensor to start up and continuously set whether motion has happened
+        - Set up camera to record when motion detected and continue recording for 15 seconds
+            after the motion has stopped. Read up on what `wait_recording()` can actually do
+        - Get shell script to instantiate this object and start up motion sensor to record when
+            motion is active
         """
-        self.motion_pin_number = motion_sensor_pin_number
-        # Set up camera
-        # Set up motion sensor to start up and continuously set whether motion has happened
-        # Set up camera to record when motion detected and continue recording for 15 seconds
-        #   after the motion has stopped. Read up on what `wait_recording()` can actually do
-        # Get shell script to instantiate this object and start up motion sensor to record when motionis active
+        # Set the mode for GPIO pins
+        GPIO.setmode(GPIO.BCM)
+
+        self.MOTION = DotDict(
+            {
+                "COUNT": 0,
+                "CURRENT": None,
+                "DETECTED": False,
+                "SENSOR": motion_sensor_pin_number,
+                # datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                "START": None,
+            }
+        )
+        self.MOTION_END_TIME_LAPSE = timedelta(seconds=15)
+
+        # Set up the input connector to check for when the motion sensor is tipped off
+        GPIO.setup(self.MOTION.SENSOR, GPIO.IN)
+
+    def _append_motion(self):
+        #  Set detected to true
+        self.MOTION.DETECTED = True
+        # Set the start date of the trip
+        self.MOTION.START = datetime.now()
+        # Append one to the motion count
+        self.MOTION.COUNT += 1
+
+    async def _detect_motion(self):
+        # Give the sensor an extra second for start up
+        await asyncio.sleep(1)
+
+        try:
+            while True:
+                # If sensor is tripped
+                if GPIO.input(self.MOTION.SENSOR):
+                    # If we haven't been tripped and this is a new motion
+                    if self.MOTION.DETECTED == False:
+                        # Append new motion
+                        self._append_motion()
+                    else:
+                        # Keep our motion current
+                        self.MOTION.CURRENT = datetime.now()
+
+                else:
+                    # Now always check if we can end a motion when we are not triggering
+                    self._end_motion()
+
+                await asyncio.sleep(0)
+        except KeyboardInterrupt:
+            print(f"Manual exit.")
+        finally:
+            # this ensures a clean exit
+            GPIO.cleanup()
+
+    def _end_motion(self):
+        # Check if we are 15 seconds ahead of our motion's current
+        delta = datetime.now() - self.MOTION.CURRENT
+        self.MOTION.DETECTED = (
+            False
+            if self.MOTION.DETECTED == True and delta > self.MOTION_END_TIME_LAPSE
+            else None
+        )
+
+        # Add an async function that kicks off a function that creates a directory, based on the current data
+        # and moves all the video footage into it
+        # NOTE: https://stackoverflow.com/questions/8858008/how-to-move-a-file-in-python
+        # shutil.move() will copy and remove the original if you move the files to a different drive,
+        # and I bet a USB would fall under this category
+
+
+class DotDict(dict):
+    """Utility code for added functionality for `dict` to access values easier/cleaner"""
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
---
+++ asynctest.py
@@ -0,0 +1,45 @@
+import asyncio
+import random
+
+
+class test:
+    def __init__(self) -> None:
+        self.test = False
+        self.testernum = 0
+
+    async def nested(self, n):
+        print(f"#{n} started")
+        await asyncio.sleep(random.random() * 10)
+        print(f"#{n} am done, test: {self.get_test()}")
+        self.set_test()
+
+    async def tester(self):
+        while self.testernum < 300000:
+            print(f"Counter: {self.testernum}, bool: {self.test}")
+            await asyncio.sleep(0)
+            self.testernum += 1
+
+    async def main(self):
+
+        # Schedule nested() to run soon concurrently
+        # with "main()".
+        task1 = asyncio.create_task(self.nested(1))
+        task2 = asyncio.create_task(self.nested(2))
+        task3 = asyncio.create_task(self.tester())
+
+        # "task" can now be used to cancel "nested()", or
+        # can simply be awaited to wait until it is complete:
+        await asyncio.wait([task1, task2, task3])
+
+    def run(self):
+        asyncio.run(self.main())
+
+    def set_test(self):
+        self.test = True
+
+    def get_test(self):
+        return self.test
+
+
+obj = test()
+obj.run()
